51fe9802a9826eb310ef684b0667ed4fbf426abf
druvesque <89401265+druvesque@users.noreply.github.com>
2024-06-13 17:36:00 +0530

    Add files via upload

---
+++ first_program.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("PW");
+    return 0;
+}
---
+++ integers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    // short int a = 10;
+    // printf("%d\n", a);
+    // printf("%u", a);
+
+    short int b = -10;
+    printf("%d\n", b);
+    printf("%hu", b);
+    return 0;
+}
